e6cf19d2a604a5b58fcc9efb629ab731744501c4
Tsvetomir Nachev <57493062+tsvetomir87@users.noreply.github.com>
2019-11-08 19:54:47 +0200

    Add files via upload

---
+++ zoo/Animal.java
@@ -0,0 +1,14 @@
+package zoo;
+
+public class Animal {
+
+    private static String name;
+
+    public Animal(String name){
+        this.name = name;
+    }
+
+    public static String getName() {
+        return name;
+    }
+}
